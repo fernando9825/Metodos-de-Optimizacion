47edee8ebe5dcadd0cf2ca57c5bbb68f9597c381
Fernando Alvarado <fer25alvarado@gmail.com>
2018-05-12 19:48:42 -0600

    Pasos 1-3
    Ya complete los pasos del 1-3, también quite unos errores en dichos pasos.

--- src/metodosimplex/MetodoSimplex.java
+++ src/metodosimplex/MetodoSimplex.java
@@ -46,12 +46,11 @@ public class MetodoSimplex {
             {3, 3, 3, 30},
             {0, 2, 5, 30},
             {4, 4, 0, 24}};
-        /*  
-            Paso 1:
-                Convertir las desigualdades en igualdades
-         */
 
- /*
+        /*  
+            PASO 1: Convertir las desigualdades en igualdades, introduciendo 
+            variables de holgura, una por cada restricción que tengamos.
+         
             debido a que este problema es con <=... para determinar cuantas variables de holguras vamos a tener...
             solo hay que ver multiplicar la cantidad de restricciones que tenemos, esto es:
             array.lenght * 2, y a eso hay que sumarle 1, para considerar a la función objetivo.
@@ -130,14 +129,21 @@ public class MetodoSimplex {
                 + "            4X1 + 4X2       <= 24\n\n");
 
         /*
+            PASO 2: Ordenar los datos en una tabla simplex. 
+        
+        
             Se definen las columnas del simplex, mediante el siguiente bucle, el
             el cual funciona obteniendo el número de filas que tiene la matriz
             "igualdades".
          */
         //System.out.println("VB     X1        X2        X3        X4        X5        X6        Bi");
-        System.out.print("VB     ");
+        String espacioEntreColumnas = "             ";
+        String espacioEntreColumnasNegativo = "            ";
+
+        System.out.println("TABLA SIMPLEX\n");
+        System.out.print("VB" + espacioEntreColumnas);
         for (int i = 1; i < igualdades[0].length; i++) {
-            System.out.print("X" + i + "        ");
+            System.out.print("X" + i + espacioEntreColumnas + " ");
         }
         System.out.print("Bi\n");
 
@@ -152,16 +158,16 @@ public class MetodoSimplex {
                         String filaVariableBase = "";
                         for (int x = 0; x < igualdades[i].length; x++) {
                             if (igualdades[i][x] < 0) {
-                                filaVariableBase += igualdades[i][x] + "      "; //Si el valor es negativo se coloca un espacio menos
+                                filaVariableBase += igualdades[i][x] + espacioEntreColumnasNegativo; //Si el valor es negativo se coloca un espacio menos
                             } else {
 
-                                filaVariableBase += igualdades[i][x] + "       ";
+                                filaVariableBase += igualdades[i][x] + espacioEntreColumnas;
                             }
                         }
 
                         //Se muestra la fila que contiene variable en la Base.
                         System.out.print("X" + (j + 1));
-                        System.out.print("     " + filaVariableBase);
+                        System.out.print(espacioEntreColumnas + filaVariableBase);
                         break;//Terminamos el bucle anterior, porque ya fue recorrido.
 
                     }
@@ -169,17 +175,17 @@ public class MetodoSimplex {
                 } else {
                     String filaZ = ""; //Se crea una nueva cadena para poder almacenar la Fila Z
                     for (int k = 0; k < igualdades[i].length; k++) { //Aqui se lee la fila Z y se concatena cada valor
-                        
-                        if (igualdades[i][j] < 0) {
-                            filaZ += igualdades[i][k] + "      "; //Si el valor es negativo se coloca un espacio menos
+
+                        if (igualdades[i][k] < 0) {
+                            filaZ += igualdades[i][k] + espacioEntreColumnasNegativo; //Si el valor es negativo se coloca un espacio menos.
                         } else {
-                            filaZ += igualdades[i][k] + "       ";
+                            filaZ += igualdades[i][k] + espacioEntreColumnas;
 
                         }
                     }
                     //Se muestra la fila Z
                     System.out.print("Z");
-                    System.out.print("      " + filaZ);
+                    System.out.print(espacioEntreColumnas + " " + filaZ);
                     break; //Terminamos el bucle anterior, porque ya fue recorrido.
                 }
 
@@ -187,5 +193,50 @@ public class MetodoSimplex {
             System.out.println("");//Dar el salto de linea para la siguiente fila
         }//Fin del primer bucle
 
+        System.out.println("\n");//Doble salto de linea, luego de terminar el paso 2.
+        /*
+            PASO 3:  Determine la solución básica factible inicial. 
+            Se seleccionan las variables holgura como variables de base inicial.
+        
+            Aquí se imprime la solución básica inicial, recorriendo la matriz 
+            igualdades, donde se encuentren los valores 1.0 en la intersección
+            de fila-columna, eso nos indica que la variable esta en la base.
+         */
+
+        //Obtenemos el valor Bi de Z
+        double Z = igualdades[(igualdades.length-1)][(igualdades[(igualdades.length-1)].length-1)];
+        
+        //Este arreglo nos sirva para almacenar los Bi de las variables básicas.
+        double[] varBasicas = new double[variablesBasicasPosI.size()];
+        for (int i = 0; i < variablesBasicasPosI.size(); i++) {
+            varBasicas[i] = igualdades[i][(igualdades[i].length - 1)];
+        }//Aquí hemos obtenido el valor de las variables básicas.    
+
+        System.out.println("SOLUCIÓN BÁSICA INICIAL");
+        //Se muestra la matriz con igualdades luego de haber convertido las desigualdades
+        for (int i = 0, k = 0; i < igualdades.length; i++) {
+            for (int j = 0; j < igualdades[i].length; j++) {
+
+                if ((i == variablesBasicasPosI.get(i)) && (j == variablesBasicasPosJ.get(i))) {
+                    //Si se entra aqui, quiere decir que esa variable esta en la base.
+                    
+                    System.out.print("X" + (j + 1) + " = " + varBasicas[k]);
+                    k++;/*si se encuentra una variable de base, se aumenta el valor de k
+                          Para poder mostrar la siguiente variable que esta en la base
+                          (si es que hay alguna)*/
+                    break;//Terminamos, porque la variable de base ya se determino.
+                }
+
+            }//Fin del segundo bucle.
+            System.out.println("");//Salto de linea
+            if(k == variablesBasicasPosI.size()){
+                break;//Salimos del bucle puesto que ya no hay más variables en la Base.
+            }
+        }//Fin del primer bucle.
+        
+        //Mostrando el valor de Z en la Solución básica inicial.
+        System.out.println("Z = " + Z + "\n\n");
+        
     }
+
 }
