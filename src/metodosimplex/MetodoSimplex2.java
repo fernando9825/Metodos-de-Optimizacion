c0f0d58f771e5c341bcd8fae69d0b88e946c1831
Fernando Alvarado <striker@striker-Satellite-L45D-C>
2018-05-26 19:17:53 -0600

    Paso 1 integrado ademas he creado el form de la UI, pero no he programado nada en ella aun

--- src/metodosimplex/MetodoSimplex2.java
+++ src/metodosimplex/MetodoSimplex2.java
@@ -18,12 +18,38 @@ public class MetodoSimplex2 {
     static Fraction fraccion = new Fraction();//Instancia de la clase Fraction para convertir decimales a quebrados
     private static int columnaPivote, filaPivote, MAXMIN;
     private static double[][] matriz;
-    private static double[] fObjetivo, x, y;
+    private static double[] fObjetivo;
     private static boolean condicionZ = false;
+     //Nuevos campos de clase
+    static ArrayList<Integer> vHolguraIndice = new ArrayList<>();
+    static ArrayList<Integer> vArtificialIndice = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
-        matriz = paso1();
+        //matriz = paso1();
+        
+        MAXMIN = 1; //MIN
+        //Restricciones
+        double[] fObjetivo = {5, 8, 7};//Función objetivo, Z = C1X1 + C2X2 + ... + CnXn
+        setfObjetivo(fObjetivo);
+        double[][] array = new double[][]{ //Restricciones, para este caso, todas <=
+            {3, 3, 3, 30},
+            {0, 2, 5, 30},
+            {4, 4, 0, 24}};
 
+        /*
+           CONDICIÓN DE LAS RESTRICCIONES:
+        
+            0 ---->  (<=)
+            1 ---->  (=)
+            2 ---->  (>=)
+         */
+        int[] condicion = new int[]{
+            0,
+            0,
+            2,};
+        
+        matriz = paso1(array, fObjetivo, condicion, MAXMIN, verificarCondiciones(condicion));
+        
         //Paso2 -->Mostrar matriz
         System.out.println("");
         System.out.println(Arrays.deepToString(matriz).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
@@ -58,110 +84,228 @@ public class MetodoSimplex2 {
         comprobarZ(matriz, fObjetivo);
     }
 
-    public static double[][] paso1() {
+    public static double[][] paso1(double[][] array, double[] fObjetivo, int[] condicion, int MAXMIN, boolean menorOIgualQue) {
         /*
-
-            Resolver mediante el método simplex:
-        
-            MAX Z = 5X1 + 8X2 +7X3
-
-            S.A.R
-                3X1 + 3X2 + 3X3 <= 30
-                    + 2X2 + 5X3 <= 30
-                4X1 + 4X2       <= 24    
-
-
-            //Creando un el arreglo de las restricciones
-            double[][] array = new double[][]{
-                {3, 3, 3, 1, 0, 0, 30},
-                {0, 2, 5, 0, 1, 0, 30},
-                {4, 4, 0, 0, 0, 1, 24},
-                {-5, -8, -7, 0, 0, 0, 0} //Función objetivo
-        }; 
+            Si menorOIgualque = true, no se ejecuta la penalización, 
+            si es false, se utiliza la Tecnica M y se penaliza la función objetivo
          */
-        //Variables para el paso 4 (que está más adelante).
-        /*
-            0 - El usuario no ha decidido si es un problema de MAX o MIN.
-            1 - Problema de Maximización
-            2 - Problema de Minización
+
+ /*
+            Esta parte de encarga de mostrar en consola, el paso 1.
+            Luego de haber convertido las desigualdades en igualdades.
          */
-        MAXMIN = 1; //MAX
+        //Problema planteado inicialmente.
+        System.out.println("MAX Z = 5X1 + 8X2 +7X3\n"
+                + "        \n"
+                + "        S.A.R\n"
+                + "            3X1 + 3X2 + 3X3 <= 30\n"
+                + "                + 2X2 + 5X3 <= 30\n"
+                + "            4X1 + 4X2       <= 24\n\n");
 
-        //Restricciones
-        double[] fObjetivo = {5, 8, 7};//Función objetivo, Z = C1X1 + C2X2 + ... + CnXn
-        setfObjetivo(fObjetivo);
-        double[][] array = new double[][]{ //Restricciones, para este caso, todas <=
-            {3, 3, 3, 30},
-            {0, 2, 5, 30},
-            {4, 4, 0, 24}};
-//double[] fObjetivo = {5, 2};//Función objetivo, Z = C1X1 + C2X2 + ... + CnXn
-//        setfObjetivo(fObjetivo);
-//        double[][] array = new double[][]{ //Restricciones, para este caso, todas <=
-//            {4, 4, 50},
-//            {1, 6, 34},
-//            {3, 3, 36}};
-//        double[] fObjetivo = {4, 6, 7, 3, 7};//Función objetivo, Z = C1X1 + C2X2 + ... + CnXn
-//        setfObjetivo(fObjetivo);
-//        double[][] array = new double[][]{ //Restricciones, para este caso, todas <=
-//            {3, 7, 5, 8, 5, 80},
-//            {7, 4, 8, 0, 0, 45},
-//            {0, 0, 3, 8, 2, 5}};
+        int indice = (array[0].length - 1);//Indice de la ultima variable de decision
 
-        /*  
-            PASO 1: Convertir las desigualdades en igualdades, introduciendo 
-            variables de holgura, una por cada restricción que tengamos.
-         
-            debido a que este problema es con <=... para determinar cuantas variables de holguras vamos a tener...
-            solo hay que ver multiplicar la cantidad de restricciones que tenemos, esto es:
-            array.lenght * 2, y a eso hay que sumarle 1, para considerar a la función objetivo.
-         */
-        //CONVIRTIENDO LAS RESTRICCIONES EN IGUALDADES, AGREGANDO UNA VARIABLE DE HOLGURA (PROBLEMA CON <=)
-        double[][] igualdades = new double[(array.length + 1)][2 * (array.length) + 1];
-        int[] varBaseX = new int[array.length];
+        //Determinando la nueva dimension para la matriz
+        int columnas = array.length, filas = array[0].length;
 
-        for (int i = 0; i < array.length; i++) { //Se recorre la matriz original (con restricciones)
+        for (int x = 0; x < condicion.length; x++) {
+            switch (condicion[x]) {
+                case 0:
+                    filas++;
+                    break;
+                case 1:
+                    filas++;
+                    break;
+                default:
+                    filas += 2;
+                    break;
+            }
+        }
+
+        //Construyendo la nueva matriz
+        double[][] aux = new double[columnas][filas];
+        for (int i = 0; i < array.length; i++) {
 
             for (int j = 0; j < array[i].length; j++) {
-
-                if (j == (array[i].length - 1)) { //La columna de bi, se convierte en la última columna de la matriz igualdades
-                    igualdades[i][(igualdades[i].length - 1)] = array[i][j];
+                if (j == (array[i].length - 1)) {
+                    aux[i][(aux[i].length - 1)] = array[i][j];
                 } else {
-
-                    igualdades[i][j] = array[i][j]; //Se agregan las variables de decisión a la nueva matriz igualdades
-
-                    if (j == array[i].length) {
-
-                        for (int x = i; x < (igualdades.length - 1); x++, j++) {
-                            igualdades[i][(j + 1)] = 1;
-
-                        }
-                    }
+                    aux[i][j] = array[i][j];
                 }
             }
         }
 
-        //En estos for anidados, se recorre la matriz igualdades para agregar las variables de holgura.
-        for (int i = 0, k = (array[i].length - 1); i < igualdades.length - 1; i++, k++) {
-            igualdades[i][k] = 1; //Se establecen las variables de holgura
+        //Agregar variable de holgura y artificiales;
+        for (int i = 0; i < aux.length; i++) {
+
+            switch (condicion[i]) {
+                case 0:
+                    aux[i][indice] = 1;
+                    vHolguraIndice.add(indice);
+                    indice++;
+
+                    break;
+                case 1:
+                    aux[i][indice] = 1;
+                    vArtificialIndice.add(indice);
+                    indice++;
+
+                    break;
+                default:
+                    aux[i][indice] = -1;
+                    vHolguraIndice.add(indice);
+                    indice++;
+                    aux[i][indice] = 1;
+                    vArtificialIndice.add(indice);
+                    indice++;
+
+                    break;
+            }
         }
+
+//        System.out.println(Arrays.deepToString(aux).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
+//        System.out.println("");
+
+        if (!vHolguraIndice.isEmpty()) {
+            System.out.println("Variables de holgura");
+            vHolguraIndice.forEach((Integer a) -> {
+                System.out.println("X" + a);
+            });
+        }
+
+        if (!vArtificialIndice.isEmpty()) {
+            System.out.println("\nVariables artificiales");
+            vArtificialIndice.forEach((Integer b) -> {
+                System.out.println("X" + b);
+            });
+        }
+
+        //Ejecutar la penalizacion en la función objetivo
+//        System.out.println("\n");
 
         /*
-            Por último se procede a hacer Z=0, en la función objetivo.
-            para lo que creamos un nuevo arreglo llamado fObjetivoCero
-            y multiplicamos cada elemento por -1, de esta manera,
-            tendremos Z de la forma Z = 0, como el método exige.
+            Para mi caso, he dividido la función objetivo en dos para poder
+            aplicar la "técnica M"-
+        
+            el vectorM, contendra todos los terminos de M
+            el vectorZ contendrá todos los terminos independientes de M
          */
-        double[] fObjetivoCero = new double[igualdades[0].length];
+        double[] vectorM = new double[filas];
+        double[] vectorZ = new double[filas];
 
-        for (int i = 0; i < fObjetivo.length; i++) {
-            fObjetivoCero[i] = fObjetivo[i] * -1;
+        //Aplicando penalización en vectorZ
+        for (int x = 0; x < vArtificialIndice.size(); x++) {
+            switch (MAXMIN) {
+                case 1:
+                    //MAXIMIZAR
+                    vectorM[vArtificialIndice.get(x)] = -1;
+
+                    break;
+
+                case 2:
+                    //MINIMIZAR
+                    vectorM[vArtificialIndice.get(x)] = 1;
+            }
         }
 
-        //Agregamos la función objetivo a la última fila de igualdades.  
-        System.arraycopy(fObjetivoCero, 0, igualdades[(igualdades.length - 1)], 0, igualdades[0].length);
+        //Estableciendo valor de vectorZ
+        System.arraycopy(fObjetivo, 0, vectorZ, 0, fObjetivo.length);
 
-        
-        return igualdades;
+//        System.out.println(Arrays.toString(vectorM));
+//        System.out.println(Arrays.toString(vectorZ));
+//        System.out.println("\n\n");
+        //Igualar vectorM y vector Z a cero; VectorM = vectorZ = 0
+        for (int x = 0; x < filas; x++) {
+            vectorM[x] *= -1;
+            vectorZ[x] *= -1;
+        }
+
+        //Limpiar vectores del valor -0.0
+        for (int x = 0; x < filas; x++) {
+            if (vectorM[x] == -0.0) {
+                vectorM[x] = 0;
+            }
+
+            if (vectorZ[x] == -0.0) {
+                vectorZ[x] = 0;
+            }
+        }
+
+//        System.out.println(Arrays.toString(vectorM));
+//        System.out.println(Arrays.toString(vectorZ));
+//        System.out.println("\n\n");
+        //Agregar la matriz aux, vectorM y vectorZ a la matriz de igualdades aux2
+        double[][] aux2 = new double[(aux.length + 2)][filas];
+
+        for (int i = 0; i < aux.length; i++) {
+            System.arraycopy(aux[i], 0, aux2[i], 0, aux[i].length);
+        }
+
+        for (int i = aux.length; i < aux2.length; i++) {
+            for (int j = 0; j < aux2[i].length; j++) {
+                if (i == aux.length) {
+                    aux2[i][j] = vectorM[j];
+                } else {
+                    aux2[i][j] = vectorZ[j];
+                }
+            }
+        }
+
+//        System.out.println(Arrays.deepToString(aux2).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
+
+        /*  System.out.println(Arrays.deepToString(aux2).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
+            Si se da el caso de que todas las restricciones sean <=
+            declaramos una nueva matriz aux3, con una sola fila z
+         */
+        double[][] aux3 = new double[(aux.length + 1)][filas];
+
+        for (int i = 0; i < aux.length; i++) {
+            System.arraycopy(aux[i], 0, aux3[i], 0, aux[i].length);
+        }
+
+        double[] fObjetivoAux = new double[fObjetivo.length];
+        for (int x = 0; x < fObjetivoAux.length; x++) {
+            fObjetivoAux[x] = fObjetivo[x] * -1;
+        }
+
+        //Limpiar la función objetivo auxiliar
+        for (int x = 0; x < fObjetivoAux.length; x++) {
+            if (fObjetivoAux[x] == -0.0) {
+                fObjetivo[x] = 0;
+            }
+        }
+
+        //Insertar fObjetivo Aux en Aux3
+        System.arraycopy(fObjetivoAux, 0, aux3[columnas], 0, fObjetivoAux.length);
+
+//        System.out.println("\n");
+//        System.out.println(Arrays.deepToString(aux3).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
+        if (menorOIgualQue) {
+            return aux3;
+        } else {
+            return aux2;
+        }
+
+    }
+    
+    public static boolean verificarCondiciones(int[] condicion) {
+        boolean menorOIgual = false;
+
+        for (int x : condicion) {
+            if (x == 0) {
+
+                menorOIgual = true;
+            } else {
+                menorOIgual = false;
+                break;
+            }
+        }
+
+        if (menorOIgual) {
+            System.out.println("Todas las restricciones son <=");
+        }else{
+            System.out.println("tenemos una combinación de diferentes tipos de restricción");
+        }
+        return menorOIgual;
     }
 
     public static void setfObjetivo(double[] fObjetivo2) {
@@ -446,12 +590,18 @@ public class MetodoSimplex2 {
 
         ArrayList<Double> divisionMenorValor = new ArrayList<>();
         for (int i = 0; i < igualdades.length - 1 /*-1 para no considerar a Z*/; i++) {
+            
+            if(igualdades[i][(filaPivote)] == 0){
+               //Si el denominador es cero, devolver infinito
+                divisionMenorValor.add(Double.POSITIVE_INFINITY);
+            }else{
             divisionMenor[i] = igualdades[i][(igualdades[0].length - 1)] / igualdades[i][(filaPivote)];
 
             System.out.println((i + 1) + " : " + fraccion.fraction(igualdades[i][(igualdades[0].length - 1)])
                     + "/" + fraccion.fraction(igualdades[i][(filaPivote)])
                     + " = " + divisionMenor[i]);
             divisionMenorValor.add(divisionMenor[i]);
+            }
         }
 
         for (int k = 0; k < divisionMenor.length; k++) {
